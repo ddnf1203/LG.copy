4b2ed0f4fc85e039ab3bdb503d4dce19fadf202f
ddnf1203 <ddnf@naver.com>
2022-05-03 19:02:10 +0900

    22-05-03-01

--- js/sctipt.js
+++ js/sctipt.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
     $(".menu-bar > li").mouseover(function(){
       $(".menu-bar > li").addClass("active")
     })
+    $(".menu-bar > li").mouseout(function(){
+        $(".menu-bar > li").removeClass("active")    
+    })
     
     
     
